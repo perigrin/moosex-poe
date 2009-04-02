d05c8d077bdde7835a3d316244ec108e5480dea3
Chris Prather <cprather@hdpublishing.com>
2009-04-02 13:06:54 -0400

    add a can() so that tests pass

--- lib/MooseX/POE/Meta/Trait/Class.pm
+++ lib/MooseX/POE/Meta/Trait/Class.pm
@@ -8,25 +8,25 @@ with qw(MooseX::Async::Meta::Trait);
 # so we can support on_ events
 
 around default_events => sub {
-    my ($next, $self) = @_;
+    my ( $next, $self ) = @_;
     my $events = $next->($self);
     push @$events, grep { s/^on_(\w+)/$1/; } $self->get_method_list;
     return $events;
 };
 
 around add_role => sub {
-  my ($next, $self, $role) = @_;
-  
-  $next->($self, $role);
-  if ( $role->meta->does_role("MooseX::Async::Meta::Trait") ) {
-      $self->add_event($role->get_events);
-  }
+    my ( $next, $self, $role ) = @_;
+    $next->( $self, $role );
+    if (   $role->meta->can('does_role')
+        && $role->meta->does_role("MooseX::Async::Meta::Trait") ) {
+        $self->add_event( $role->get_events );
+    }
 };
 
 around get_state_method_name => sub {
     my ( $next, $self, $name ) = @_;
     return 'on_' . $name if $self->has_method( 'on_' . $name );
-    return $next->($self, $name);
+    return $next->( $self, $name );
 };
 
 sub get_all_events {
@@ -36,11 +36,15 @@ sub get_all_events {
     # This horrible grep can be removed once Moose gets more metacircular.
     # Currently Moose::Meta::Class->meta isn't a MMC. It should be, and it
     # should also be a Moose::Object so does works on it.
-    my %events = 
-      map { my $m = $_; map { $_ => $m->get_state_method_name($_) } $m->get_events } 
-      grep { $_->meta->can('does_role') && $_->meta->does_role($wanted_role) } 
-      map { $_->meta } 
-      $self->linearized_isa;
+    my %events
+        = map {
+        my $m = $_;
+        map { $_ => $m->get_state_method_name($_) } $m->get_events
+        }
+        grep {
+        $_->meta->can('does_role') && $_->meta->does_role($wanted_role)
+        }
+        map { $_->meta } $self->linearized_isa;
 }
 
 no Moose::Role;
