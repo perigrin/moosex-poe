d308d296a58ecc03091fcfa6f5eb204ebc014ef2
Chris Williams <chris@bingosnet.co.uk>
2009-06-19 14:29:37 +0100

    Added MooseX::POE testcase

---
+++ mxtestcase.pl
@@ -0,0 +1,50 @@
+use Test::More tests => 2;
+
+package mxpoe;
+
+use MooseX::POE;
+use MooseX::AttributeHelpers;
+use Test::More;
+
+sub START {
+  my ($kernel,$self) = @_[KERNEL,OBJECT];
+  diag "Starting .... \n";
+  $kernel->yield( 'counter_event' );
+  return;
+}
+
+event 'counter_event' => sub {
+  my ($kernel,$self) = @_[KERNEL,OBJECT];
+  pass('Got a counter event');
+  return;
+};
+
+no MooseX::POE;
+
+__PACKAGE__->meta->make_immutable;
+
+package mxpoet;
+
+use MooseX::POE;
+use Test::More;
+
+extends 'mxpoe';
+
+sub START {
+  my ($kernel,$self) = @_[KERNEL,OBJECT];
+  diag "Starting EXTENSION .... \n";
+  $kernel->yield( 'counter_event' );
+  return;
+}
+
+no MooseX::POE;
+
+package main;
+use strict;
+use warnings;
+use POE;
+
+my $mxpoe = mxpoet->new();
+
+$poe_kernel->run();
+exit 0;
