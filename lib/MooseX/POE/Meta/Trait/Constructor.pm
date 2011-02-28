6e9f21addaa63179ea9c39c67d1eefadb4d6c04d
Dave Rolsky <autarch@urth.org>
2011-02-28 11:19:03 -0600

    Don't wrap a non-existent method with newer Moose

--- lib/MooseX/POE/Meta/Trait/Constructor.pm
+++ lib/MooseX/POE/Meta/Trait/Constructor.pm
@@ -8,31 +8,33 @@ sub _expected_method_class { "MooseX::POE::Meta::Trait::Object" }
 # For older versions of Moose/CMOP
 sub _expected_constructor_class { "MooseX::POE::Meta::Trait::Object" }
 
-around _generate_instance => sub {
-    my $orig = shift;
-    my ($self, $var, $class_var) = @_;
+if ( $Moose::VERSION < 1.9900 ) {
+    around _generate_instance => sub {
+        my $orig = shift;
+        my ($self, $var, $class_var) = @_;
 
-    my %events = $self->associated_metaclass->get_all_events;
-    $events{STARTALL} = 'STARTALL';
-    $events{_stop} = 'STOPALL';
+        my %events = $self->associated_metaclass->get_all_events;
+        $events{STARTALL} = 'STARTALL';
+        $events{_stop} = 'STOPALL';
 
-    my $events = join(', ', map { 
-      s/'/\\'/g;
-      "'$_'"
-    } %events);
+        my $events = join(', ', map { 
+          s/'/\\'/g;
+          "'$_'"
+        } %events);
 
-    my $source = $orig->(@_) . <<"EOF"
-my \$session = POE::Session->create(
-    inline_states => { _start => sub { POE::Kernel->yield('STARTALL', \$_[5] ) }, },
-    object_states => [
-      $var => { $events }
-    ],
-    args => [ $var ],
-    heap => (${var}->{heap} ||= {}),
-);
-${var}->{session_id} = \$session->ID;
+        my $source = $orig->(@_) . <<"EOF"
+    my \$session = POE::Session->create(
+        inline_states => { _start => sub { POE::Kernel->yield('STARTALL', \$_[5] ) }, },
+        object_states => [
+          $var => { $events }
+        ],
+        args => [ $var ],
+        heap => (${var}->{heap} ||= {}),
+    );
+    ${var}->{session_id} = \$session->ID;
 EOF
-};
+    };
+}
 
 no Moose::Role;
 
